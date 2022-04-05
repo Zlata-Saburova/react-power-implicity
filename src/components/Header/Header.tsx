4eb57dc8094d65c396b63545fda3800df1cf9d49
Zlata <zlatasaburova97@gmail.com>
2022-04-05 23:04:50 +0300

    feat(header): add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,35 @@
 import "./styles.scss";
 
 export const Header = () => {
-  return;
+  return (
+    <header className="header">
+      <div className="header__slider">
+        <h1 className="header__title">The Power of Simplicity</h1>
+        <p className="header__text">
+          Instead of spending time searching for the right app, our AI will
+          bring the right app to you.
+        </p>
+        <a href="" className="header__link">
+          Learn
+        </a>
+        <ul className="header__dotsList">
+          <li className="header__item">
+            <div className="header__dot"></div>
+          </li>
+          <li className="header__item">
+            <div className="header__dot"></div>
+          </li>
+          <li className="header__item">
+            <div className="header__dot header__dot--active"></div>
+          </li>
+          <li className="header__item">
+            <div className="header__dot"></div>
+          </li>
+          <li className="header__item">
+            <div className="header__dot"></div>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
